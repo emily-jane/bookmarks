2f6139eea4ac411cc7d968811946d29f430a60c3
Haroon <haroon.hassan@gmail.com>
2015-08-12 15:28:03 +0100

    helper methods done

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,15 @@ require_relative '../data_mapper_setup'
 
 class Bookmarks < Sinatra::Base
 
+enable :sessions
+set :session_secret, 'super secret'
+
+helpers do
+  def current_user
+    User.get(session[:user_id])
+  end 
+end
+
   get '/' do
   redirect '/links/new'
   end
@@ -25,14 +34,25 @@ class Bookmarks < Sinatra::Base
   end
 
   get '/links/new' do
+    test
     erb :'links/new'
   end
  
   get '/tags/:name' do
 
     tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : [] # brilliant refactoring
+    @links = tag ? tag.links : [] 
     erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/'
   end
 
 end
--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -1,6 +1,7 @@
 require 'data_mapper'
 require './app/models/link'
 require './app/models/tags'
+require './app/models/user'
 
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
